4890497b6958d0125132ded7e66e8ba9309056d0
bodysen <liang.ys@qq.com>
2024-01-18 15:42:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://ai.micropdf.top', // 后台的地址
+        target: 'http://43.136.45.112', // 后台的地址
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
