d6231584de45e086d8ecbb8b7f241df3b426e662
bodysen <liang.ys@qq.com>
2024-01-26 10:40:58 +0800

    Update Header.tsx

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useLocation, Link } from 'react-router-dom';
-import { MoonIcon, Sun, Laptop, Languages, Github } from 'lucide-react';
+import { MoonIcon, Sun, Laptop, Languages } from 'lucide-react';
 
 import { useAppStore, ThemeModeType, LanguagesType, useUserStore } from '@/store';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
