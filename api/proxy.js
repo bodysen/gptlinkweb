ea051ebf2c8ab4ec4487b919b83f2fef9ebe565d
bodysen <liang.ys@qq.com>
2024-02-02 09:37:56 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ const {
     createProxyMiddleware
   } = require('http-proxy-middleware')
 
-export const BACKEND_HOST = import.meta.env.API_HOST;
+export const BACKEND_HOST = process.env.API_HOST;
 
   module.exports = (req, res) => {
     let target = ''
