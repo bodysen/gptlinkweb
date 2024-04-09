e1152d68e8c652339c1937e4ad670f1249fc1935
bodysen <liang.ys@qq.com>
2024-04-09 14:30:47 +0800

    Update index.tsx iframe

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,6 +1,14 @@
 //import Chat from './Chat';
 //import Conversation from './Conversation';
 //import { useMobileScreen } from '@/hooks/use-mobile-screen';
+import { useUserStore} from '@/store';
+
+useEffect(() => {
+    const iframe = document.querySelector('iframe');
+    const [{ nickname, avatar }] = useUserStore((state) => [state.userInfo]);
+    const userInfo = { nickname, avatar };
+    iframe.contentWindow.postMessage(userInfo, 'https://ai.micropdf.top');
+}, [userInfo]);
 
 export default function Home() {
   //const isMobileScreen = useMobileScreen();
