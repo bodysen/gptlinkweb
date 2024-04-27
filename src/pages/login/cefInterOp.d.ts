3469c9b32d615a5d248f00a6d285836861dbb7e1
bodysen <liang.ys@qq.com>
2024-04-27 10:30:00 +0800

    Update cefInterOp.d.ts

--- src/pages/login/cefInterOp.d.ts
+++ src/pages/login/cefInterOp.d.ts
@@ -1,9 +1,1 @@
-interface CefInterOp {
-    scanSuccess: () => void;
-}
 declare function recvRenderMsg(arg1: any, arg2: any): void;
-declare global {
-    interface Window {
-        cefInterOp?: CefInterOp;
-    }
-}
